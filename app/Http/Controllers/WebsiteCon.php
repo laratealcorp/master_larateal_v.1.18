9340fbbf8745a26dd690807a13fd249a4955ea3d
LaraTeal <127891037+laratealcorp@users.noreply.github.com>
2023-03-16 14:06:16 +0800

    Update WebsiteCon.php

--- app/Http/Controllers/WebsiteCon.php
+++ app/Http/Controllers/WebsiteCon.php
@@ -9,9 +9,19 @@ class WebsiteCon extends Controller
 {
     public function home(){
         if(inc('web_status',1)=='true'){
-            return abort(404, 'HALAMAN BELUN DI BUAT');
+            $m = num_rows('moduls',['class'=>'web','status'=>'true']);
+            if($m==1){
+                $m = get_row('moduls',['class'=>'web','status'=>'true']);
+                $data['m']=$m;
+                $mig = num_and_get('tb_'.$m->id);
+                $data['mig'] = ($mig)?$mig:[];
+                return view('costume/'.$m->id.'/index',$data);
+            }else{
+                return abort(404);
+            }
         }else{
             return redirect('login');
         }
     }
 }
+
